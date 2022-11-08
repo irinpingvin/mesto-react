744d72fd48d4f494c04774c8bfd1a456044bee3d
Irina Alyabeva <irinpingviiin@yandex.ru>
2022-11-08 23:50:56 +0300

    Проектная работа 10
    Реализовано получение данных о пользователе с сервера

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,36 @@
+import React from "react";
 import Footer from "./Footer.js";
+import {api} from "../utils/Api.js";
 
 function Main(props) {
+  const [userInfo, setUserInfo] = React.useState({userName: '', userDescription: '', userAvatar: ''});
+
+  React.useEffect(() => {
+    api.getUserInfo().then(data => {
+      setUserInfo({
+        userName: data.name,
+        userDescription: data.about,
+        userAvatar: data.avatar
+      })
+    })
+    .catch(error => console.log(error));
+  })
+
   return (
     <main className="content">
       <section className="profile section">
         <div className="profile__avatar-container">
-          <img className="profile__avatar"/>
+          <img className="profile__avatar" style={{ backgroundImage: `url(${userInfo.userAvatar})` }}/>
           <div className="profile__avatar-overlay">
             <button className="profile__avatar-edit-button" type="button" onClick={props.onEditAvatar}></button>
           </div>
         </div>
         <div className="profile__personal-data">
           <div className="profile__name-editor">
-            <h1 className="profile__name"></h1>
+            <h1 className="profile__name">{userInfo.userName}</h1>
             <button className="profile__edit-button" type="button" onClick={props.onEditProfile}></button>
           </div>
-          <p className="profile__description"></p>
+          <p className="profile__description">{userInfo.userDescription}</p>
         </div>
         <button className="profile__add-button" type="button" onClick={props.onAddPlace}></button>
       </section>
