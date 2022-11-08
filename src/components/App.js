fdddef44d13e5d0d289d9f1351cba2b2cc2cca06
Irina Alyabeva <irinpingviiin@yandex.ru>
2022-11-08 22:36:08 +0300

    Проектная работа 10
    Реализация закрытия popup

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,9 @@ import PopupWithForm from './PopupWithForm.js';
 import ImagePopup from "./ImagePopup.js";
 
 function App() {
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
-  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
 
   function handleEditAvatarClick() {
     setIsEditAvatarPopupOpen(true);
@@ -21,12 +21,18 @@ function App() {
     setIsAddPlacePopupOpen(true);
   }
 
+  function closeAllPopups() {
+    setIsEditAvatarPopupOpen(false);
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+  }
+
   return (
     <div className="page">
       <div className="page__container">
         <Header/>
         <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick}/>
-        <PopupWithForm title='Редактировать профиль' name='profile' isOpen={isEditProfilePopupOpen}>
+        <PopupWithForm title='Редактировать профиль' name='profile' isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
           <input type="text" name="name" required className="popup__input popup__input_text_name" id="name-input"
                  minLength="2" maxLength="40"/>
           <span className="popup__input-error name-input-error"></span>
@@ -36,7 +42,7 @@ function App() {
           <button type="submit" className="popup__submit-button">Сохранить</button>
         </PopupWithForm>
 
-        <PopupWithForm title='Новое место' name='card' isOpen={isAddPlacePopupOpen}>
+        <PopupWithForm title='Новое место' name='card' isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
           <input type="text" name="title" required className="popup__input popup__input_text_name"
                  id="card-name-input" placeholder="Название" minLength="2" maxLength="30"/>
           <span className="popup__input-error name-input-error card-name-input-error"></span>
@@ -52,7 +58,7 @@ function App() {
           <button type="submit" className="popup__submit-button">Да</button>
         </PopupWithForm>
 
-        <PopupWithForm title='Обновить аватар' name='avatar' isOpen={isEditAvatarPopupOpen}>
+        <PopupWithForm title='Обновить аватар' name='avatar' isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
           <input type="url" name="avatar" required className="popup__input popup__input_text_info"
                  id="avatar-url-input" placeholder="Ссылка на картинку"/>
           <span className="popup__input-error info-input-error avatar-url-input-error"></span>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,7 +2,7 @@ function PopupWithForm(props) {
   return (
     <div className={`popup popup_type_${props.name} ${props.isOpen ? 'popup_opened' : ''}`}>
       <div className="popup__container popup__container_type_user-input">
-        <button className="popup__close-button" type="button"></button>
+        <button className="popup__close-button" type="button" onClick={props.onClose}></button>
         <h3 className="popup__title">{props.title}</h3>
         <form className="popup__form" name={`${props.name}-form`} noValidate>
           {props.children}
